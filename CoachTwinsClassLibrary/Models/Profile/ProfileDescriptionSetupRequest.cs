46786c56ad676101cce9ab4cc717fb2ee1039349
Arsemi337 <arturmilosz@onet.pl>
2022-04-07 15:41:59 +0200

    new model for the description setup added

---
+++ CoachTwinsClassLibrary/Models/Profile/ProfileDescriptionSetupRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoachTwinsMobileApp.ClassLibrary.Models.Profile
+{
+    public class ProfileDescriptionSetupRequest
+    {
+        public string Description { get; set; }
+    }
+}
